e330636f4cc5cc68033471a80993d5a09ee795a0
Nilson <netohjunior216@gmail.com>
2025-11-12 19:23:07 -0300

    feat: add initial application structure with main components and styles

---
+++ frontend/src/main.jsx
@@ -0,0 +1,28 @@
+import './index.css'
+
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import { BrowserRouter, Route, Routes } from 'react-router'
+import { Toaster } from 'sonner'
+
+import HomePage from './pages/home.jsx'
+import LoginPage from './pages/login.jsx'
+import NotFoundPage from './pages/not-found.jsx'
+import SignupPage from './pages/signup.jsx'
+
+const queryClient = new QueryClient()
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <QueryClientProvider client={queryClient}>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/signup" element={<SignupPage />} />
+        </Routes>
+      </BrowserRouter>
+      <Toaster />
+    </QueryClientProvider>
+  </StrictMode>
+)
