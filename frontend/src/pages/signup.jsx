86c78c8d3c53243995d7cfd574bc08dede42e19b
Nilson <netohjunior216@gmail.com>
2025-11-14 15:00:05 -0300

    feat: refactor signup page to use authentication context and simplify form handling

--- frontend/src/pages/signup.jsx
+++ frontend/src/pages/signup.jsx
@@ -1,9 +1,7 @@
 import { zodResolver } from '@hookform/resolvers/zod'
-import { useMutation } from '@tanstack/react-query'
-import { useEffect, useState } from 'react'
+import { useContext, } from 'react'
 import { useForm } from 'react-hook-form'
 import { Link } from 'react-router-dom'
-import { toast } from 'sonner'
 import { z } from 'zod'
 
 import PasswordInput from '@/components/password-input'
@@ -27,7 +25,8 @@ import {
 } from '@/components/ui/form'
 import { Input } from '@/components/ui/input'
 import { Label } from '@/components/ui/label'
-import { api } from '@/lib/axios'
+import { AuthContext } from '@/contexts/auth'
+
 
 const signupSchema = z.object({
   firstName: z.string().trim().min(1, { message: 'O nome é obrigatório' }),
@@ -58,19 +57,8 @@ const signupSchema = z.object({
 
 
 const SignupPage = () => {
-  const [user, setUser] = useState(null)
-  const signupMutation = useMutation({
-    mutationKey: ['signup'],
-    mutationFn: async (variables) => {
-      const response = await api.post('/users', {
-        first_name: variables.firstName,
-        last_name: variables.lastName,
-        email: variables.email,
-        password: variables.password,
-      })
-      return response.data
-    },
-  })
+  const { user, signup } = useContext(AuthContext)
+
   const form = useForm({
     resolver: zodResolver(signupSchema),
     defaultValues: {
@@ -82,45 +70,14 @@ const SignupPage = () => {
       termsAccepted: false,
     },
   })
-  useEffect(() => {
-    const init = async () => {
-      try {
-      const accessToken = localStorage.getItem('accessToken')
-      const refreshToken = localStorage.getItem('refreshToken')
-      if (!accessToken && !refreshToken) return
-      const response = await api.get('/users/me', {
-        headers: {
-          Authorization: `Bearer ${accessToken}`,
-        },
-      })
-      setUser(response.data) 
-      } catch(error) {
-        localStorage.removeItem('accessToken')
-        localStorage.removeItem('refreshToken')
-        console.error('Error retrieving tokens from localStorage', error)
-      } 
-    }
-    init()
-  }, [])
+  
 
-  const handleSubmit = (data) => {
-    signupMutation.mutate(data, {
-      onSuccess: (createdUser) => {
-        const accesToken = createdUser.access_token
-        const refreshToken = createdUser.refresh_token
-        setUser(createdUser)
-        localStorage.setItem('accessToken', accesToken)
-        localStorage.setItem('refreshToken', refreshToken)
-        toast.success('Conta criada com sucesso!')
-      },
-      onError: () => {
-        toast.error('Erro ao criar a conta. Tente novamente.')
-      },
-    })
-  }
+  const handleSubmit = (data) => signup(data)
+  
   if (user) {
-    return <h1>Olá, {user.first_name}!</h1>
+     return <h1>Ola, {user.first_name}</h1>
   }
+
   return (
     <div className="flex h-screen w-screen flex-col items-center justify-center gap-3">
       <Form {...form}>
