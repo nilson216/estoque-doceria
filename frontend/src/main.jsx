be6f5f00e21c98a2ed1baff75b2592efc2a44f7d
Nilson <netohjunior216@gmail.com>
2025-11-14 14:59:22 -0300

    fix: correct import statement for react-router and adjust component structure in main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,9 +3,10 @@ import './index.css'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { BrowserRouter, Route, Routes } from 'react-router'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import { Toaster } from 'sonner'
 
+import { AuthContextProvider } from './contexts/auth.jsx'
 import HomePage from './pages/home.jsx'
 import LoginPage from './pages/login.jsx'
 import NotFoundPage from './pages/not-found.jsx'
@@ -15,16 +16,18 @@ const queryClient = new QueryClient()
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <QueryClientProvider client={queryClient}>
+  <QueryClientProvider client={queryClient}>
+    <AuthContextProvider>
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<HomePage />} />
+          <Route path="/homepage" element={<HomePage />} />
           <Route path="/login" element={<LoginPage />} />
           <Route path="/signup" element={<SignupPage />} />
           <Route path="*" element={<NotFoundPage />} />
         </Routes>
       </BrowserRouter>
       <Toaster />
-    </QueryClientProvider>
-  </StrictMode>
+    </AuthContextProvider>
+  </QueryClientProvider>
+</StrictMode>
 )
