8f828c13710eea115da549dea66b226f957abe1a
Nilson <netohjunior216@gmail.com>
2025-11-25 09:25:59 -0300

    feat: reorganize imports and clean up code structure in app.js

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,8 @@
 import 'dotenv/config.js'
-
 import { app } from './src/app.js'
 
 const port = process.env.PORT || 3000;
 
 app.listen(port, () =>
-    console.log(`Listening on port ${port}`),
+    console.log(`Listening on port ${port}`)
 );
--- backend/src/app.js
+++ backend/src/app.js
@@ -1,27 +1,36 @@
 import express from 'express';
-import { usersRouter, ingredientsRouter } from './routes/index.js';
-import { movementsRouter } from './routes/movements.js';
-import swaggerUi from 'swagger-ui-express';
+import cors from 'cors';
 import fs from 'fs';
 import path from 'path';
+import swaggerUi from 'swagger-ui-express';
 import { fileURLToPath } from 'url';
+
+import { usersRouter, ingredientsRouter } from './routes/index.js';
+import { movementsRouter } from './routes/movements.js';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
+
+// Carrega o Swagger
 const openApiSpec = JSON.parse(
-	fs.readFileSync(path.join(__dirname, '../docs/swagger.json'), 'utf8')
+    fs.readFileSync(path.join(__dirname, '../docs/swagger.json'), 'utf8')
 );
-import cors from 'cors';
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
+// Rotas da API
+app.get('/', (req, res) => {
+    res.json({ message: "API funcionando! 🚀" });
+});
+
 app.use('/api/users', usersRouter);
 app.use('/api/ingredients', ingredientsRouter);
 app.use('/api/movements', movementsRouter);
 
+// Swagger
 app.use('/api/docs', swaggerUi.serve, swaggerUi.setup(openApiSpec));
 
 export { app };
