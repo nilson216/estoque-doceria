b28f396c28053b539cd5011ad30e5fa791f3828e
Nilson <netohjunior216@gmail.com>
2025-11-25 08:03:11 -0300

    feat: add userId parameter to DeleteMovementUseCase for ownership enforcement

--- backend/src/use-cases/movement/delete-movement.js
+++ backend/src/use-cases/movement/delete-movement.js
@@ -4,6 +4,10 @@ export class DeleteMovementUseCase {
     }
 
     async execute(id) {
+        if (arguments.length >= 2) {
+            const userId = arguments[1];
+            return await this.deleteMovementRepository.execute(id, userId);
+        }
         return await this.deleteMovementRepository.execute(id);
     }
 }
