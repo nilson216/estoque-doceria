1b9860996d5625f114c96ddac836fe6b9bb14fb9
Nilson <netohjunior216@gmail.com>
2025-11-25 08:03:19 -0300

    feat: allow passing userId to GetMovementByIdUseCase for enhanced access control

--- backend/src/use-cases/movement/get-movement-by-id.js
+++ backend/src/use-cases/movement/get-movement-by-id.js
@@ -4,6 +4,11 @@ export class GetMovementByIdUseCase {
     }
 
     async execute(id) {
+        // allow passing userId as second arg (from controller)
+        if (arguments.length >= 2) {
+            const userId = arguments[1];
+            return await this.getMovementByIdRepository.execute(id, userId);
+        }
         return await this.getMovementByIdRepository.execute(id);
     }
 }
